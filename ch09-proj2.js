97f631a3c2a75479bb405c1a333935ae531bfaa0
jrdhaliwal <jrdhaliwal101@gmail.com>
2023-11-21 23:59:58 -0700

    Finished

--- ch09-proj2.js
+++ ch09-proj2.js
@@ -9,22 +9,56 @@ document.addEventListener("DOMContentLoaded", function() {
         const img = document.createElement("img");
 
         img.src = `images/small/${d.id}.jpg`;
-        let paintingID = d.id;
-        li.id = paintingID;
+        img.dataset.id = d.id;
 
         li.appendChild(img);
         listContainer.appendChild(li);
     }
 
     node.addEventListener("click", function(e) {
-        if(e.target.nodeName == "LI") {
-
+        if(e.target.nodeName == "IMG") {
+            fill(e);
         }
     })
 
     function fill(event) {
-        const figureContainer = document.querySelector("figure").innerHTML = "";
-        const id = event.target.id;
+        const figureContainer = document.querySelector("figure");
+        figureContainer.innerHTML = "";
+        const id = event.target.dataset.id;
         const selectedID = data.find(data => data.id == id)
+
+        figureContainer.innerHTML = `<img src="images/large/${id}.jpg">`;
+        document.querySelector("#title").textContent = selectedID.title;
+        document.querySelector("#artist").textContent = selectedID.artist;
+        
+        //let num = 0;
+
+        for(let feature of selectedID.features) {
+            const div = document.createElement("div");
+            div.className = "box";
+        
+            const left = feature.upperLeft[0];
+            const top = feature.upperLeft[1];
+            const width = feature.lowerRight[0] - left;
+            const height = feature.lowerRight[1] - top;
+
+            div.style.position = "absolute";
+            div.style.left = `${left}px`;
+            div.style.top = `${top}px`;
+            div.style.width = `${width}px`;
+            div.style.height = `${height}px`;
+
+            div.addEventListener("mouseover", function(e) {
+                document.querySelector("#description").textContent = feature.description;
+            })
+
+            div.addEventListener("mouseout", function(e) {
+                document.querySelector("#description").textContent = "";
+            }) 
+
+            figureContainer.appendChild(div);
+        }
+
+
     }
 })
